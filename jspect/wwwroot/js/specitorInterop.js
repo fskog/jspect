1c3838718308dab2ae5e6394dfda95799f6a6cf5
FS <>
2019-05-08 00:22:03 +0200

    Add project files.

---
+++ jspect/wwwroot/js/specitorInterop.js
@@ -0,0 +1,10 @@
+﻿window.Specitor = {
+    parseHtmlListAsyncJs: function (dotnetHelper) {
+        var editor = document.getElementById('specitor_input')
+        var input = editor.innerHTML;
+        dotnetHelper.invokeMethodAsync('ParseHtmlListAsync', input)
+            .then(data => {
+                console.log("ParseHtmlListAsync returned." + data);
+            });
+    }
+};
